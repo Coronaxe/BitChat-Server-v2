62841bd14930b4931cceb0d00c3b91d66c301040
Coronaxe <viktorfuchs@gmx.at>
2015-12-24 00:07:29 +0100

    Update version 0.85, Add DDoS Protection, Add Passwort Security, Improve Code

--- src/at/coro/network/connection/Server.java
+++ src/at/coro/network/connection/Server.java
@@ -26,15 +26,28 @@ public class Server implements Runnable {
 	protected ADEC cryptoEngine;
 	protected PublicKey clientPublicKey;
 
+	protected final String serverAnnounce = "[SERVER]";
+	protected final String serverInfo = "SRV_INFO>>";
 	protected String username = "user_" + Math.round(Math.random() * 1000);
+	protected final int uuid = (int) Math.round(Math.random() * 10000000);
+	protected String ipAddress = "";
+	protected String password = "";
 
-	public Server(Socket socket) throws IOException {
-		System.out
-				.println("--------------------\nNew incoming client connection!\nAddress is: "
-						+ socket.getRemoteSocketAddress());
+	public Server(Socket socket, String password) throws IOException {
+		System.out.println("--------------------\nNew incoming client connection!\nAddress is: "
+				+ socket.getRemoteSocketAddress());
+		System.out.println("Username is: " + this.username);
+		this.ipAddress = socket.getRemoteSocketAddress().toString();
+		this.password = password;
 		this.clientSocket = socket;
 		this.oos = new ObjectOutputStream(this.clientSocket.getOutputStream());
 		this.ois = new ObjectInputStream(this.clientSocket.getInputStream());
+	}
+
+	public void disconnect() throws IOException {
+		// sendEncryptedMessage("");
+		this.clientSocket.close();
+		return;
 	}
 
 	public String getUsername() {
@@ -45,16 +58,20 @@ public class Server implements Runnable {
 		this.oos.writeObject(message);
 	}
 
-	public void sendEncryptedMessage(Object message)
-			throws InvalidKeyException, IllegalBlockSizeException,
-			BadPaddingException, NoSuchAlgorithmException,
-			NoSuchPaddingException, IOException {
-		this.oos.writeObject(cryptoEngine.encryptString(clientPublicKey,
-				message.toString()));
+	public void sendEncryptedMessage(Object message) throws InvalidKeyException, IllegalBlockSizeException,
+			BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, IOException {
+		this.oos.writeObject(cryptoEngine.encryptString(clientPublicKey, message.toString()));
 	}
 
 	public Object listen() throws ClassNotFoundException, IOException {
-		return ois.readObject();
+		return listen(0);
+	}
+
+	public Object listen(int timeout) throws ClassNotFoundException, IOException {
+		this.clientSocket.setSoTimeout(timeout);
+		Object returnObj = ois.readObject();
+		this.clientSocket.setSoTimeout(0);
+		return returnObj;
 	}
 
 	@Override
@@ -65,17 +82,39 @@ public class Server implements Runnable {
 			cryptoEngine = new ADEC(2048);
 			System.out.println("Done!");
 			// Exchanging keys
+			System.out.println("Waiting for Public Key...");
+			clientPublicKey = (PublicKey) listen(5000);
 			System.out.println("Sending Public Key...");
 			sendMessage(cryptoEngine.publicKey());
-			System.out.println("Waiting for Public Key...");
-			clientPublicKey = (PublicKey) listen();
-			System.out.println("Success!\n--------------------");
 			//
-		} catch (KeyException | NoSuchAlgorithmException
-				| NoSuchProviderException | IOException
-				| ClassNotFoundException e) {
+			if (!this.password.isEmpty()) {
+				System.out.println("Requesting password...");
+				if (!this.password.equals(cryptoEngine.decryptString((byte[]) listen(10000)))) {
+					throw new Exception("WRONG_PASSWORD");
+				}
+			}
+			System.out.println("Success!\n--------------------");
+			sendEncryptedMessage(this.serverInfo + "CORRECT_PASSWORD");
+			mainThread.broadcastMessage(this.serverAnnounce + ": A NEW USER JOINED THE FUN: " + this.username);
+		} catch (IOException ioex) {
+			System.err.println("IO Error occurred! Maybe there was a timeout!");
+			return;
+			// System.err.println();
+		} catch (KeyException | NoSuchAlgorithmException | NoSuchProviderException | ClassNotFoundException
+				| IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException e) {
 			System.err.println("An Error Occurred!\nCause: " + e.getMessage());
 			return;
+		} catch (Exception ex) {
+			if (ex.getMessage().equals("WRONG_PASSWORD")) {
+				System.err.println("Wrong Password!");
+				try {
+					sendEncryptedMessage(this.serverInfo + "WRONG_PASSWORD");
+				} catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException
+						| NoSuchAlgorithmException | NoSuchPaddingException | IOException e) {
+					e.printStackTrace();
+				}
+				return;
+			}
 		}
 
 		do {
@@ -84,40 +123,31 @@ public class Server implements Runnable {
 			try {
 
 				clientMessage = cryptoEngine.decryptString((byte[]) listen());
-				System.out.println(username + "("
-						+ this.clientSocket.getRemoteSocketAddress() + "): "
-						+ clientMessage);
-				if (clientMessage.toUpperCase().startsWith("/BRD")) {
-					mainThread.broadcastMessage("<"
-							+ this.username
-							+ ">: "
-							+ clientMessage.substring(
-									clientMessage.indexOf(" ") + 1,
-									clientMessage.length()));
+				System.out.println(username + "(" + this.clientSocket.getRemoteSocketAddress() + "): " + clientMessage);
+				if (clientMessage.toUpperCase().startsWith("/HELP")) {
+					String commands = "/count : Returns count of users online\n/chusr [username] : Changes your current username to the specified one\n/users : Returns the usernames currently logged in";
+					sendEncryptedMessage(this.serverAnnounce + ": COMMAND LIST:\n" + commands);
+				} else if (clientMessage.toUpperCase().startsWith("/BRD")
+						&& !clientMessage.substring(clientMessage.indexOf(" ") + 1, clientMessage.length()).isEmpty()) {
+					mainThread.broadcastMessage("<" + this.username + ">: "
+							+ clientMessage.substring(clientMessage.indexOf(" ") + 1, clientMessage.length()));
 				} else if (clientMessage.toUpperCase().startsWith("/CHUSR")) {
 					String user = clientMessage.split(" ")[1];
-					if (!mainThread.userExists(user)
-							&& !user.equalsIgnoreCase("SERVER")) {
-						mainThread.broadcastMessage("<SERVER>: "
-								+ this.username
-								+ " IS NOW KNOWN UNDER THE NAME " + user);
+					if (!mainThread.userExists(user) && !user.toUpperCase().contains(this.serverAnnounce.toUpperCase())
+							&& !user.equalsIgnoreCase("ALL")) {
+						mainThread.broadcastMessage(
+								this.serverAnnounce + ": " + this.username + " IS NOW KNOWN UNDER THE NAME " + user);
 						this.username = user;
 					} else {
-						sendEncryptedMessage("<SERVER>: USERNAME " + user
-								+ " IS ALREADY TAKEN, CHOOSE ANOTHER ONE!");
+						sendEncryptedMessage(
+								this.serverAnnounce + ": USERNAME " + user + " IS ALREADY TAKEN, CHOOSE ANOTHER ONE!");
 					}
 				} else if (clientMessage.toUpperCase().startsWith("/COUNT")) {
-					sendEncryptedMessage("<SERVER>: USERS ONLINE: "
-							+ mainThread.userCount());
+					sendEncryptedMessage(this.serverAnnounce + ": USERS ONLINE: " + mainThread.userCount());
 				} else if (clientMessage.toUpperCase().startsWith("/SAY")) {
 					String recipient = clientMessage.split(" ")[1];
-					String messageBody = "<"
-							+ this.username
-							+ ">: "
-							+ clientMessage.substring(
-									clientMessage.indexOf(recipient)
-											+ recipient.length() + 1,
-									clientMessage.length());
+					String messageBody = "<" + this.username + ">: " + clientMessage.substring(
+							clientMessage.indexOf(recipient) + recipient.length() + 1, clientMessage.length());
 					mainThread.directMessage(recipient, messageBody);
 				} else if (clientMessage.toUpperCase().startsWith("/USERS")) {
 					String[] onlineUsers = mainThread.getUsers();
@@ -125,12 +155,11 @@ public class Server implements Runnable {
 					for (int i = 0; i < onlineUsers.length; i++) {
 						userList += onlineUsers[i] + " | ";
 					}
-					sendEncryptedMessage("<SERVER>: USERS: " + userList);
+					sendEncryptedMessage(this.serverAnnounce + ": USERS: " + userList);
 				}
 
-			} catch (ClassNotFoundException | IOException | InvalidKeyException
-					| NoSuchAlgorithmException | NoSuchPaddingException
-					| IllegalBlockSizeException | BadPaddingException e) {
+			} catch (ClassNotFoundException | IOException | InvalidKeyException | NoSuchAlgorithmException
+					| NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
 				// System.err.println("An Error Occurred!\nCause: " +
 				// e.getMessage());
 				break;
@@ -145,10 +174,16 @@ public class Server implements Runnable {
 			this.clientSocket.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			System.err
-					.println("Something went wrong while cleaning up! We're still fine though!");
+			System.err.println("Something went wrong while cleaning up! We're still fine though!");
 		}
-		System.out.println("User Disconnected.");
+		System.out.println("User " + this.username + " Disconnected.");
+		try {
+			mainThread.broadcastMessage("User " + this.username + " says Goodbye!");
+		} catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | NoSuchAlgorithmException
+				| NoSuchPaddingException | IOException e) {
+			// System.out.println("FATAL ERROR:");
+			// e.printStackTrace();
+		}
 		return;
 
 	}
--- src/at/coro/run/Main.java
+++ src/at/coro/run/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -20,18 +21,17 @@ import at.coro.utils.ConfigManager;
 
 public class Main {
 
-	public static final String version = "0.8b";
+	public final String version = "0.85";
 	private static final String configPath = "config.ini";
 
 	Properties configuration = null;
 	private static ArrayList<Object[]> clientThreads = new ArrayList<Object[]>();
 
-	private Properties configure(String configPath, boolean skipIO)
-			throws IOException {
+	private Properties configure(String configPath, boolean skipIO, boolean rewriteConfig) throws IOException {
 
 		ConfigManager confMan = new ConfigManager(configPath);
 
-		if (!skipIO && confMan.configExists()) {
+		if (!rewriteConfig && (!skipIO && confMan.configExists())) {
 			return confMan.loadConfig();
 		} else {
 			Properties configuration = new Properties();
@@ -39,6 +39,10 @@ public class Main {
 			configuration.setProperty("Server_Socket_Port", "6988");
 			configuration.setProperty("Broadcast_Interval", "5000");
 			configuration.setProperty("MOTD", "Chitchat on BitChat!");
+			configuration.setProperty("Version", this.version);
+			configuration.setProperty("DDoSProtection", "true");
+			configuration.setProperty("DDoSProtection_Cutoff", "5");
+			configuration.setProperty("Password", "");
 
 			if (!skipIO) {
 				confMan.saveConfig(configuration);
@@ -65,8 +69,7 @@ public class Main {
 			public void run() {
 				UDPDiscovery udpDiscover = new UDPDiscovery();
 				while (true) {
-					udpDiscover.broadcast(this.broadcastMessage,
-							this.broadcastPort);
+					udpDiscover.broadcast(this.broadcastMessage, this.broadcastPort);
 					try {
 						Thread.sleep(this.broadcastInterval);
 					} catch (InterruptedException e) {
@@ -86,8 +89,48 @@ public class Main {
 				System.out.println("Thread " + i + " dead!\nDisposing...");
 				// ((Thread) clientThreads.get(i)[1]).stop();
 				Main.clientThreads.remove(i);
-				System.out.println("Active Threads: "
-						+ Main.clientThreads.size());
+				System.out.println("Active Threads: " + Main.clientThreads.size());
+			}
+		}
+	}
+
+	private boolean ddosProtection(SocketAddress address) {
+		int counter = 0;
+
+		for (int i = 0; i < clientThreads.size(); i++) {
+			if (((SocketAddress) clientThreads.get(i)[2]).toString().split(":")[0]
+					.equals(address.toString().split(":")[0])) {
+				counter++;
+			}
+			if (counter >= Integer.parseInt(this.configuration.getProperty("DDoSProtection_Cutoff"))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private int getClientThread(String user) {
+		for (int i = 0; i < Main.clientThreads.size(); i++) {
+			if (((Server) Main.clientThreads.get(i)[0]).getUsername().equalsIgnoreCase(user)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private void disconnect(String user) throws IOException {
+		if (user.equals("all")) {
+			for (int i = Main.clientThreads.size() - 1; i >= 0; i--) {
+				System.out.println("Disconnecting " + ((Server) Main.clientThreads.get(i)[0]).getUsername() + "...");
+				((Server) Main.clientThreads.get(i)[0]).disconnect();
+				Main.clientThreads.remove(i);
+			}
+		} else {
+			if (userExists(user)) {
+				System.out.println("Disconnecting " + user + "...");
+				((Server) Main.clientThreads.get(getClientThread(user))[0]).disconnect();
+			} else {
+				System.err.println("NO SUCH USER!");
 			}
 		}
 	}
@@ -95,6 +138,7 @@ public class Main {
 	private void inputHandler() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
+			System.out.print(">");
 			String command = null;
 			try {
 				command = br.readLine();
@@ -103,35 +147,42 @@ public class Main {
 			}
 			command = command.trim();
 			if (command.toUpperCase().startsWith("/QUIT")) {
+				try {
+					disconnect("all");
+				} catch (IOException e) {
+				}
+				System.out.println("Stopping Main Thread...");
 				System.exit(0);
 			} else if (command.toUpperCase().startsWith("/HELP")) {
 				showCommands();
 			} else if (command.toUpperCase().startsWith("/BRD")) {
 				try {
-					broadcastMessage("<SERVER>: "
-							+ command.substring(command.indexOf(" ") + 1,
-									command.length()));
-				} catch (IOException | InvalidKeyException
-						| IllegalBlockSizeException | BadPaddingException
+					broadcastMessage("<SERVER>: " + command.substring(command.indexOf(" ") + 1, command.length()));
+				} catch (IOException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException
 						| NoSuchAlgorithmException | NoSuchPaddingException e) {
 					e.printStackTrace();
 				}
 			} else if (command.toUpperCase().startsWith("/INFO")) {
-				System.out.println("There are currently " + userCount()
-						+ " users online;");
+				System.out.println("There are currently " + userCount() + " users online;");
 				String[] onlineUsers = getUsers();
 				System.out.print("| ");
 				for (int i = 0; i < onlineUsers.length; i++) {
 					System.out.print(onlineUsers[i] + " | ");
 				}
 				System.out.println();
+			} else if (command.toUpperCase().startsWith("/DISCONNECT")) {
+				try {
+					disconnect(command.split(" ")[1]);
+				} catch (IOException e) {
+					System.out.println("Error while disconnecting users!");
+				}
 			} else if (command.toUpperCase().startsWith("/MOTD")) {
 				System.out.println("To be implemented");
 			} else {
-				System.err
-						.println("No such command! Type /help for a list of available commands!");
+				System.err.println("No such command! Type /help for a list of available commands!");
 			}
 		}
+
 	}
 
 	private void showCommands() {
@@ -140,6 +191,8 @@ public class Main {
 		System.out.println("/quit : Quits the server.");
 		System.out.println("/info : Lists user count and usernames");
 		System.out.println("/brd [Message] : Broadcasts message to all users");
+		System.out.println(
+				"/disconnect [user] : Disconnects specified user. Type \"all\" instead to disconnect everyody");
 		System.out.println("------------------------------------------------");
 	}
 
@@ -151,8 +204,7 @@ public class Main {
 
 	public boolean userExists(String username) {
 		for (int i = 0; i < Main.clientThreads.size(); i++) {
-			if (((Server) Main.clientThreads.get(i)[0]).getUsername()
-					.equalsIgnoreCase(username)) {
+			if (((Server) Main.clientThreads.get(i)[0]).getUsername().equalsIgnoreCase(username)) {
 				return true;
 			}
 		}
@@ -162,32 +214,24 @@ public class Main {
 	public String[] getUsers() {
 		String[] userList = new String[Main.clientThreads.size()];
 		for (int i = 0; i < Main.clientThreads.size(); i++) {
-			userList[i] = (((Server) Main.clientThreads.get(i)[0])
-					.getUsername());
+			userList[i] = (((Server) Main.clientThreads.get(i)[0]).getUsername());
 		}
 		return userList;
 
 	}
 
-	public void broadcastMessage(String message) throws IOException,
-			InvalidKeyException, IllegalBlockSizeException,
-			BadPaddingException, NoSuchAlgorithmException,
-			NoSuchPaddingException {
+	public void broadcastMessage(String message) throws IOException, InvalidKeyException, IllegalBlockSizeException,
+			BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
 		for (int i = 0; i < Main.clientThreads.size(); i++) {
-			((Server) Main.clientThreads.get(i)[0])
-					.sendEncryptedMessage(message);
+			((Server) Main.clientThreads.get(i)[0]).sendEncryptedMessage(message);
 		}
 	}
 
-	public void directMessage(String username, String message)
-			throws InvalidKeyException, IllegalBlockSizeException,
-			BadPaddingException, NoSuchAlgorithmException,
-			NoSuchPaddingException, IOException {
+	public void directMessage(String username, String message) throws InvalidKeyException, IllegalBlockSizeException,
+			BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, IOException {
 		for (int i = 0; i < Main.clientThreads.size(); i++) {
-			if (((Server) Main.clientThreads.get(i)[0]).getUsername()
-					.equalsIgnoreCase(username)) {
-				((Server) Main.clientThreads.get(i)[0])
-						.sendEncryptedMessage(message);
+			if (((Server) Main.clientThreads.get(i)[0]).getUsername().equalsIgnoreCase(username)) {
+				((Server) Main.clientThreads.get(i)[0]).sendEncryptedMessage(message);
 			}
 		}
 	}
@@ -201,29 +245,35 @@ public class Main {
 		System.out.println("Loading configuration...");
 
 		try {
-			mainThread.configuration = mainThread.configure(configPath, false);
+			mainThread.configuration = mainThread.configure(configPath, false, false);
+			if (Double.parseDouble(mainThread.configuration.getProperty("Version")) < Double
+					.parseDouble(mainThread.version)) {
+				System.out.println("Version change detected, rewriting config...");
+				mainThread.configuration = mainThread.configure(configPath, false, true);
+			}
 		} catch (IOException ioEx) {
 			System.err
 					.println("Could not Write config, Probably due to a Read-Only File System. Skipping IO-Section...");
 			try {
-				mainThread.configuration = mainThread.configure(configPath,
-						true);
+				mainThread.configuration = mainThread.configure(configPath, true, false);
 			} catch (IOException fallbackError) {
-				System.err
-						.println("Fatal Error occurred in fallback mode, giving up...");
+				System.err.println("Fatal Error occurred in fallback mode, giving up...");
 				System.exit(0);
 			}
 		}
+		mainThread.configuration.list(System.out);
 
-		System.out.println("Starting UDP Broadcast...");
+		System.out.println("Starting UDP Broadcast Thread...");
 		// Sets up a Thread to broadcast the server announce message over UDP
 		// (Port 2920).
-		Thread udpBroadcastThread = udpBroadcast("BITCHAT_UDP_DISCOVER:"
-				+ mainThread.configuration.getProperty("MOTD"),
-				Integer.parseInt(mainThread.configuration
-						.getProperty("Broadcast_Interval")),
-				Integer.parseInt(mainThread.configuration
-						.getProperty("UDP_Broadcast_Port")));
+		boolean password = true;
+		if (mainThread.configuration.getProperty("Password").isEmpty()) {
+			password = false;
+		}
+		Thread udpBroadcastThread = udpBroadcast(
+				"BITCHAT_UDP_DISCOVER:PASSWORD=" + password + ":" + mainThread.configuration.getProperty("MOTD"),
+				Integer.parseInt(mainThread.configuration.getProperty("Broadcast_Interval")),
+				Integer.parseInt(mainThread.configuration.getProperty("UDP_Broadcast_Port")));
 		// Sets Thread as a daemon, exits when Main Thread exits.
 		udpBroadcastThread.setDaemon(true);
 		// Starts the Thread.
@@ -231,7 +281,7 @@ public class Main {
 
 		// Handles the input in a separate Thread, as Socket.accept() blocks
 		// the Main Thread.
-		System.out.println("Starting inputHandler...");
+		// System.out.println("Starting InputHandler Thread...");
 		Thread inputHandlerThread = new Thread(new Runnable() {
 
 			@Override
@@ -246,7 +296,7 @@ public class Main {
 
 		// The LifeGuard Thread checks the clientThread List every second for
 		// dead Threads and disposes them.
-		System.out.println("Starting LifeGuard Thread");
+		System.out.println("Starting LifeGuard Thread...");
 		Thread lifeGuardThread = new Thread(new Runnable() {
 
 			@Override
@@ -266,26 +316,39 @@ public class Main {
 		lifeGuardThread.setDaemon(true);
 		lifeGuardThread.start();
 
+		// System.out.println("Startup complete!");
+
 		ServerSocket serverSocket = null;
 		try {
 			serverSocket = new ServerSocket(
-					Integer.parseInt(mainThread.configuration
-							.getProperty("Server_Socket_Port")));
+					Integer.parseInt(mainThread.configuration.getProperty("Server_Socket_Port")));
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(2);
 		}
+		// System.out.println("Ready.");
+		Socket clientSocket = null;
 		while (true) {
 			try {
-				Socket clientSocket = serverSocket.accept();
-				Object[] client = new Object[2];
-				client[0] = new Server(clientSocket);
-				client[1] = new Thread((Runnable) client[0]);
-				Main.clientThreads.add(client);
-				((Thread) Main.clientThreads.get(Main.clientThreads.size() - 1)[1])
-						.start();
+				clientSocket = serverSocket.accept();
+				System.out.println(clientSocket.getRemoteSocketAddress());
+				if (mainThread.ddosProtection(clientSocket.getRemoteSocketAddress())) {
+					clientSocket.close();
+				} else {
+					Object[] client = new Object[3];
+					client[0] = new Server(clientSocket, mainThread.configuration.getProperty("Password"));
+					client[1] = new Thread((Runnable) client[0]);
+					client[2] = (SocketAddress) clientSocket.getRemoteSocketAddress();
+					Main.clientThreads.add(client);
+					((Thread) Main.clientThreads.get(Main.clientThreads.size() - 1)[1]).start();
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
+				try {
+					clientSocket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 			}
 		}
 
